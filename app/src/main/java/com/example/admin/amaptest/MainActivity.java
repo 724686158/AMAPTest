ad38dcf95d62854101fc568e14d15ad0ee97468a
mengzicheng <724686158@qq.com>
2016-09-17 15:44:22 +0800

    学习控件交互

--- app/src/main/java/com/example/admin/amaptest/MainActivity.java
+++ app/src/main/java/com/example/admin/amaptest/MainActivity.java
@@ -1,22 +1,47 @@
 package com.example.admin.amaptest;
 
+import android.os.health.UidHealthStats;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.SupportMapFragment;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.offlinemap.OfflineMapCity;
+import com.amap.api.maps.offlinemap.OfflineMapManager;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements LocationSource {
 
-    private AMap mMap;
+    private AMap aMap;
+    private UiSettings mUiSetting;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMap = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-        mMap.setMapType(AMap.MAP_TYPE_NORMAL);
-        mMap.setTrafficEnabled(true);
+        aMap = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        aMap.setMapType(AMap.MAP_TYPE_NORMAL);
+        //mMap.setMapType(AMap.MAP_TYPE_NIGHT);
+        aMap.setMapType(AMap.MAP_TYPE_SATELLITE);
+        aMap.setTrafficEnabled(true);
+        mUiSetting = aMap.getUiSettings();
+        mUiSetting.setCompassEnabled(true);
+        aMap.setLocationSource(this);
+        mUiSetting.setMyLocationButtonEnabled(true);
+        aMap.setMyLocationEnabled(true);
+        mUiSetting.setScaleControlsEnabled(true);
+
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener listener) {
+
+    }
+
+    @Override
+    public void deactivate() {
 
     }
 }
